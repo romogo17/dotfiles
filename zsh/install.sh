7fa822f71088e94189bf892233c592784c21d546
Roberto Mora <romogo17@gmail.com>
2020-04-06 10:30:00 -0600

    Minikube is not a cask. Fixed bug in zsh/install.sh and moved nvm vars from path to zshrc

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,7 +5,7 @@
 # This installs some of the common dependencies needed for ZSH
 
 # Check for ZSH and install if we don't have it
-if test ! $(which zsh); then
+if test $(which zsh); then
 
   echo "Installing Oh My Zsh for you"
   sh -c "KEEP_ZSHRC=yes; $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -8,10 +8,5 @@ export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 # rust
 export PATH=$HOME/.cargo/bin:$PATH
 
-#nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 #anaconda
 export PATH="$HOME/opt/anaconda3/bin:$PATH"
