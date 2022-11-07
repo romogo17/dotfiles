012c400ec61ca9a5317224697cf2299505aa970a
Roberto Mora <romogo17@gmail.com>
2022-11-07 13:35:25 -0600

    Added packages to homebrew and java_home to path

--- roles/terminal/files/fish/conf.d/path.fish
+++ roles/terminal/files/fish/conf.d/path.fish
@@ -7,6 +7,10 @@ set -gx GOPATH $HOME/go
 fish_add_path $GOROOT/bin
 fish_add_path $GOPATH/bin
 
+# Java
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home
+fish_add_path $JAVA_HOME/bin
+
 # Rust
 fish_add_path $HOME/.cargo/bin
 
