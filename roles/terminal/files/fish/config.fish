f0aa78cb579116160903e73266e48cf4cb4a90f4
Roberto Mora <romogo17@gmail.com>
2025-07-15 11:46:47 -0600

    feat: add creane to tools

--- roles/terminal/files/fish/config.fish
+++ roles/terminal/files/fish/config.fish
@@ -4,3 +4,17 @@ direnv hook fish | source
 just --completions fish | source
 zoxide init --cmd cd fish | source
 gpgconf --launch gpg-agent
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda3/bin/conda
+    eval /opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
